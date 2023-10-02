fc6df93c8c7ef8922d5543ffa57d95011d39ee41
Jonathan Adithya Baswara <jonathan-dotcom>
2023-10-02 20:06:06 +0700

    menambahkan about us

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,5 +4,19 @@ import Navbar from "../components/Navbar.vue";
 
 <template>
   <Navbar />
-  <div>this is about us page</div>
+  <h1>Siklus pengerjaan kami saat mengerjakan tugas ini : </h1>
+  <div class="memes">
+            <img src="../assets/14492556_334003136954632_7952619733944439374_n.jpg" alt="memes">
+  </div>
 </template>
+
+<style>
+.memes {
+  width: 100%;
+}
+
+.memes img {
+  width: 100%; /* Stretch the image to fill the width of its container */
+  height: auto; /* Maintain the aspect ratio */
+}
+</style>
